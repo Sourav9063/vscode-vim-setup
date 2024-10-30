a0130fe74894dbef39c83115b9ecf4c18e26bc05
Sourav <ahmed@shellbeehaken.com>
2024-10-30 14:02:58 +0600

    nvim setup

---
+++ nvim/nvim/lua/plugins/flash.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        char = {
+          enabled = false,
+          jump_labels = true
+        }
+      },
+      label = {
+        uppercase = false,
+      },
+      labels = "hjkl;yuiasdfopnmqwertzxcvb,'gHJKLYUIASDFOPNMQWERTZXCVBG"
+    },
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, false },
+      { "S",     mode = { "n", "x", "o" }, false },
+      { "f",     mode = { "n", "x", "o" }, false },
+      { "F",     mode = { "n", "x", "o" }, false },
+      { "t",     mode = { "n", "x", "o" }, false },
+      { "T",     mode = { "n", "x", "o" }, false },
+      { "f",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "F",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+      {
+        "zj",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = { mode = "search", max_length = 0, forward = true, wrap = false, multi_window = false },
+            label = { after = { 0, 0 } },
+            pattern = "^"
+          })
+        end,
+        desc = "Flash jump line"
+      },
+      {
+        "zk",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = { mode = "search", max_length = 0, forward = false, wrap = false, multi_window = false },
+            label = { after = { 0, 0 } },
+            pattern = "^"
+          })
+        end,
+        desc = "Flash jump line"
+      },
+    },
+
+  }
+}
