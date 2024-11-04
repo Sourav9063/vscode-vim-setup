4e019283dcf093a6684711d2bfec683e676f2036
Sourav <ahmed@shellbeehaken.com>
2024-11-04 11:26:01 +0600

    backup

--- nvim/nvim/lua/config/keymaps.lua
+++ nvim/nvim/lua/config/keymaps.lua
@@ -23,4 +23,5 @@ else
   vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
   vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 end
-vim.keymap.set("n", "<Esc>", ":nohlsearch<cr>")
+
+vim.keymap.set("n", "<Esc>", ":noh<cr>")
