062ef2f2594a40e8cfea787e6a510a18e0b9c6ad
Sourav <ahmed@shellbeehaken.com>
2024-11-19 11:39:09 +0600

    treesitter flash

--- nvim/nvim/lua/plugins/flash.lua
+++ nvim/nvim/lua/plugins/flash.lua
@@ -22,7 +22,7 @@ return {
       { "S",     mode = { "n", "x", "o" }, false },
       { "f",     mode = { "n", "x", "o" }, false },
       { "F",     mode = { "n", "x", "o" }, false },
-      { "t",     mode = { "n", "x", "o" }, false },
+      { "t",     mode = { "n", "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "T",     mode = { "n", "x", "o" }, false },
       { "f",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
       { "F",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
